5ca263ca2dbbf348ccc400bc4d0e2beb4b74e345
Lê Phước Hoàng <lephuochoang98tcv@gmail.com>
2021-07-06 09:54:55 +0700

    Config Webconfig

---
+++ src/main/java/com/library/Library/security/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.library.Library.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket LibraryApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(getApiInfo());
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfoBuilder()
+                .title("Library API")
+                .version("1.0")
+                .description("API for Library Application")
+                .contact(new Contact("Le Phuoc Hoang", "https://www.linkedin.com/in/le-phuoc-hoang-10933b194/", "lephuochoang98tcv@gmail.com"))
+                .license("Apache License Version 2.0")
+                .build();
+    }
+}
